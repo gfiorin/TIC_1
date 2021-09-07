cbaab2a621f1f8dce60dcee172f44f1430956f26
Juan Ignacio Ascione <juanignacio.ascione@gmail.com>
2021-09-06 22:07:02 -0300

    Cambios en el script

--- src/main/resources/mysql/tic1_db.sql
+++ src/main/resources/mysql/tic1_db.sql
@@ -3,6 +3,9 @@ CREATE SCHEMA `tic1_db` ;
 CREATE TABLE `tic1_db`.`users` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
+  `surname` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
