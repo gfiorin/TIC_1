3d112a72a8bf77061842c86f6b438c0200b0fdc1
gfiorin <gfiorina@correo.um.edu.uy>
2021-09-20 20:30:01 -0300

    Tourist

---
+++ src/main/java/com/example/AppPrototipo/business/entities/Country.java
@@ -0,0 +1,19 @@
+package com.example.AppPrototipo.business.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
