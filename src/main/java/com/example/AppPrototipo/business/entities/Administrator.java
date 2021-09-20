c587968dbb8a948d4a81a16db87dd153ff43dc0b
gfiorin <gfiorina@correo.um.edu.uy>
2021-09-20 20:23:22 -0300

    Administrador y cambio de date of birth de string a localdate

---
+++ src/main/java/com/example/AppPrototipo/business/entities/Administrator.java
@@ -0,0 +1,9 @@
+package com.example.AppPrototipo.business.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administrators")
+public class Administrator extends User{
+}
