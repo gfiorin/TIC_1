bc8be69ed4286a2eeaf4421084503b45f2e07653
Juan Cruz <juancruzcarrau1@gmail.com>
2021-09-23 09:39:11 -0300

    Creacion de intereses

---
+++ src/main/java/com/example/AppPrototipo/persistence/InterestRepository.java
@@ -0,0 +1,13 @@
+package com.example.AppPrototipo.persistence;
+
+import com.example.AppPrototipo.business.entities.Interest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InterestRepository extends CrudRepository<Interest, Integer> {
+
+    List<Interest> findAll();
+}
