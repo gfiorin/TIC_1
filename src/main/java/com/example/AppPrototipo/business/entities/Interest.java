464ffa7afed4c43c0406d6900af98347fc12f04f
Juan Cruz <juancruzcarrau1@gmail.com>
2021-10-06 17:13:01 -0300

    Correcciones login

--- src/main/java/com/example/AppPrototipo/business/entities/Interest.java
+++ src/main/java/com/example/AppPrototipo/business/entities/Interest.java
@@ -1,6 +1,7 @@
 package com.example.AppPrototipo.business.entities;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="interests")
@@ -14,9 +15,9 @@ public class Interest {
     @Column(name="name")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name="type_of_experience")
-    private ExperienceType experienceType;
+    @OneToMany()
+    @JoinColumn(name="interest")
+    private List<ExperienceType> experienceType;
 
     public Interest() {
     }
