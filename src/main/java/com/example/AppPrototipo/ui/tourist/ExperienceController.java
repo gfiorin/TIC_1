0f093b2499661229743827c022fb0199d3c44531
Juan Cruz <juancruzcarrau1@gmail.com>
2021-10-10 19:27:41 -0300

    Populacion de campos

--- src/main/java/com/example/AppPrototipo/ui/tourist/ExperienceController.java
+++ src/main/java/com/example/AppPrototipo/ui/tourist/ExperienceController.java
@@ -5,6 +5,7 @@ import com.example.AppPrototipo.persistence.ExperienceRepository;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
@@ -12,6 +13,12 @@ import java.net.URL;
 
 @Component
 public class ExperienceController {
+
+    @FXML
+    Text nombreExperiencia;
+
+    @FXML
+    Text ubicacion;
 
     @FXML
     ImageView imageViewPrincipal;
@@ -31,6 +38,11 @@ public class ExperienceController {
         imageViewMarker.setImage(imageMarker);
 
         Experience experience = experienceRepository.findById(1);
+
+        //Populacion de campos
+        nombreExperiencia.setText(experience.getTitle());
+
+        //Populacion de imagen
         Image image = new Image(new ByteArrayInputStream(experience.getImages().get(0).getImageData()));
         imageViewPrincipal.setImage(image);
     }
