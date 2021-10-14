0fb3a77fdc41e2d9cd93244e425584fee531f76f
Juan Cruz <juancruzcarrau1@gmail.com>
2021-10-14 20:43:11 -0300

    Modificaciones en script

--- src/main/resources/mysql/tic1_db.sql
+++ src/main/resources/mysql/tic1_db.sql
@@ -380,13 +380,13 @@ CREATE TABLE `experiences` (
                                CONSTRAINT `fk_tourist_operator` FOREIGN KEY (`tour_operator`) REFERENCES `tour_operators` (`id`)
 );
 
-INSERT INTO tic1_db.experiences (id,tour_operator,price,department,authorized,title,description,vaccination,capacity,bookable) VALUES
+INSERT INTO tic1_db.experiences (id,tour_operator,price,location,department,authorized,title,description,vaccination,capacity,bookable) VALUES
 (1,1,1000.00,'23 Kingston Drive Lawrence Township, NJ 08648',1,1,'Titulo de experiencia','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut lacus quis libero dignissim euismod. Duis rhoncus risus eu risus consectetur sagittis. Nulla facilisi. Maecenas nec dui et turpis vulputate lobortis in eget enim. Morbi libero velit, bibendum ac erat et, rutrum gravida nibh. Maecenas bibendum varius fringilla. Sed imperdiet, mauris eget consequat gravida, sem lacus sagittis quam, eget efficitur sapien urna vitae lorem. Sed laoreet lacinia dui, sed vulputate sapien sollicitudin eget. Mauris sollicitudin ac elit at placerat.',1,'100',1);
 
 CREATE TABLE `images` (
                           `id` int NOT NULL AUTO_INCREMENT,
                           `experience` int NOT NULL,
-                          `image` blob NOT NULL,
+                          `image` longblob NOT NULL,
                           PRIMARY KEY (`id`),
                           CONSTRAINT `fk_idexperience` FOREIGN KEY (`experience`) REFERENCES `experiences` (`id`)
 );
