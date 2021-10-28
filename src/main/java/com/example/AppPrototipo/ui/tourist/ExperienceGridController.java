c79da6f1e80a6c6fdb9674b3733218979a8aca1d
Juan Ignacio Ascione <juanignacio.ascione@gmail.com>
2021-10-27 23:18:41 -0300

    Actualizacion de menu principal

--- src/main/java/com/example/AppPrototipo/ui/tourist/ExperienceGridController.java
+++ src/main/java/com/example/AppPrototipo/ui/tourist/ExperienceGridController.java
@@ -3,6 +3,7 @@ package com.example.AppPrototipo.ui.tourist;
 import com.example.AppPrototipo.AppPrototipoApplication;
 import com.example.AppPrototipo.business.ExperienceMgr;
 import com.example.AppPrototipo.business.entities.Experience;
+import com.example.AppPrototipo.business.entities.Interest;
 import com.example.AppPrototipo.business.entities.Tourist;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -27,7 +29,31 @@ public class ExperienceGridController implements Initializable {
     private final MiniExperienceController miniExperienceController;
 
     @FXML
-    private GridPane grillaExperiencias;
+    private GridPane grillaInteres1;
+
+    @FXML
+    private GridPane grillaInteres2;
+
+    @FXML
+    private GridPane grillaInteres3;
+
+    @FXML
+    private GridPane grillaInteres4;
+
+    @FXML
+    private GridPane grillaRecomendaciones;
+
+    @FXML
+    private Text interes1;
+
+    @FXML
+    private Text interes2;
+
+    @FXML
+    private Text interes3;
+
+    @FXML
+    private Text interes4;
 
     public ExperienceGridController(ExperienceMgr experienceMgr, MiniExperienceController miniExperienceController) {
         this.experienceMgr = experienceMgr;
@@ -37,20 +63,94 @@ public class ExperienceGridController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        ArrayList<Experience> experiences = new ArrayList<>(recomendations());
+        List<Interest> interests = tourist.getInterests();
+
+        /*for(Interest interest : interests){
+        }*/
+
+        /*interes1.setText(interests.get(0).getName());
+        interes2.setText(interests.get(1).getName());
+        interes3.setText(interests.get(2).getName());
+        interes4.setText(interests.get(3).getName());*/
+
+        ArrayList<Experience> recomendations = new ArrayList<>(recomendations());
+        ArrayList<Experience> interes1 = new ArrayList<>(interes1());
+        ArrayList<Experience> interes2 = new ArrayList<>(interes2());
+        ArrayList<Experience> interes3 = new ArrayList<>(interes3());
+        ArrayList<Experience> interes4 = new ArrayList<>(interes4());
 
         int columns = 0;
         int row = 1;
 
         try {
-            for (int i=0; i < experiences.size(); i++){
+            for (int i=0; i < recomendations.size(); i++){
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setControllerFactory(AppPrototipoApplication.getContext()::getBean);
                 fxmlLoader.setLocation(miniExperienceController.getClass().getResource("MiniExperience.fxml"));
                 MiniExperienceController.setTourist(tourist);
                 VBox vbox = fxmlLoader.load();
-                miniExperienceController.setData(experiences.get(i));
-                grillaExperiencias.add(vbox,columns++,row);
+                miniExperienceController.setData(recomendations.get(i));
+                grillaRecomendaciones.add(vbox,columns++,row);
+                GridPane.setMargin(vbox,new Insets(10));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (int i=0; i < interes1.size(); i++){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setControllerFactory(AppPrototipoApplication.getContext()::getBean);
+                fxmlLoader.setLocation(miniExperienceController.getClass().getResource("MiniExperience.fxml"));
+                MiniExperienceController.setTourist(tourist);
+                VBox vbox = fxmlLoader.load();
+                miniExperienceController.setData(interes1.get(i));
+                grillaInteres1.add(vbox,columns++,row);
+                GridPane.setMargin(vbox,new Insets(10));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (int i=0; i < interes2.size(); i++){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setControllerFactory(AppPrototipoApplication.getContext()::getBean);
+                fxmlLoader.setLocation(miniExperienceController.getClass().getResource("MiniExperience.fxml"));
+                MiniExperienceController.setTourist(tourist);
+                VBox vbox = fxmlLoader.load();
+                miniExperienceController.setData(interes2.get(i));
+                grillaInteres2.add(vbox,columns++,row);
+                GridPane.setMargin(vbox,new Insets(10));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (int i=0; i < interes3.size(); i++){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setControllerFactory(AppPrototipoApplication.getContext()::getBean);
+                fxmlLoader.setLocation(miniExperienceController.getClass().getResource("MiniExperience.fxml"));
+                MiniExperienceController.setTourist(tourist);
+                VBox vbox = fxmlLoader.load();
+                miniExperienceController.setData(interes3.get(i));
+                grillaInteres3.add(vbox,columns++,row);
+                GridPane.setMargin(vbox,new Insets(10));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (int i=0; i < interes4.size(); i++){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setControllerFactory(AppPrototipoApplication.getContext()::getBean);
+                fxmlLoader.setLocation(miniExperienceController.getClass().getResource("MiniExperience.fxml"));
+                MiniExperienceController.setTourist(tourist);
+                VBox vbox = fxmlLoader.load();
+                miniExperienceController.setData(interes4.get(i));
+                grillaInteres4.add(vbox,columns++,row);
                 GridPane.setMargin(vbox,new Insets(10));
             }
         } catch (IOException e) {
@@ -65,6 +165,42 @@ public class ExperienceGridController implements Initializable {
     private List<Experience> recomendations(){
         List<Experience> list = new ArrayList<>();
         for (int i=1; i<6; i++) {
+            Experience experience = experienceMgr.findById(i);
+            list.add(experience);
+        }
+        return list;
+    }
+
+    private List<Experience> interes1(){
+        List<Experience> list = new ArrayList<>();
+        for (int i=1; i<6; i++) {
+            Experience experience = experienceMgr.findById(i);
+            list.add(experience);
+        }
+        return list;
+    }
+
+    private List<Experience> interes2(){
+        List<Experience> list = new ArrayList<>();
+        for (int i=1; i<4; i++) {
+            Experience experience = experienceMgr.findById(i);
+            list.add(experience);
+        }
+        return list;
+    }
+
+    private List<Experience> interes3(){
+        List<Experience> list = new ArrayList<>();
+        for (int i=1; i<5; i++) {
+            Experience experience = experienceMgr.findById(i);
+            list.add(experience);
+        }
+        return list;
+    }
+
+    private List<Experience> interes4(){
+        List<Experience> list = new ArrayList<>();
+        for (int i=1; i<2; i++) {
             Experience experience = experienceMgr.findById(i);
             list.add(experience);
         }
