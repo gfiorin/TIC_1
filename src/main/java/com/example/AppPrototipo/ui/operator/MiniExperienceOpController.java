8c059a30b98785b1e8d2c1f0a3629c3f30e7f2e4
marcoZunino <mzunino@correo.um.edu.uy>
2021-11-21 21:43:08 -0300

    ajuste de vista de experiencias

--- src/main/java/com/example/AppPrototipo/ui/operator/MiniExperienceOpController.java
+++ src/main/java/com/example/AppPrototipo/ui/operator/MiniExperienceOpController.java
@@ -2,9 +2,12 @@ package com.example.AppPrototipo.ui.operator;
 
 import com.example.AppPrototipo.business.entities.Experience;
 import com.example.AppPrototipo.business.managers.ExperienceMgr;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +22,9 @@ public class MiniExperienceOpController {
     private Text nombreExperiencia;
 
     @FXML
+    private Text descripcion;
+
+    @FXML
     private Text cantidadReservas;
 
     @FXML
@@ -26,6 +32,9 @@ public class MiniExperienceOpController {
 
     @FXML
     private ImageView experienciaImg;
+
+    @FXML
+    private Pane imagePane;
 
     public MiniExperienceOpController(ExperienceMgr experienceMgr) {
         this.experienceMgr = experienceMgr;
@@ -41,10 +50,43 @@ public class MiniExperienceOpController {
 
         nombreExperiencia.setText(experience.getTitle());
         cantidadReservas.setText(String.valueOf(experience.getBookings().size()));
+        descripcion.setText(String.valueOf(experience.getShortDescription()));
         estado.setText(e);
         if (!experience.getImages().isEmpty()){
             Image image = new Image(new ByteArrayInputStream(experience.getImages().get(0).getImageData()));
+            ChangeListener<Number> listener = getListener(imagePane, experienciaImg, image);
+            imagePane.widthProperty().addListener(listener);
+            imagePane.heightProperty().addListener(listener);
             experienciaImg.setImage(image);
         }
     }
+
+    private ChangeListener<Number> getListener(Pane imagePane, ImageView imageViewPrincipal, Image image) {
+        double oldImageWidth = image.getWidth(), oldImageHeight = image.getHeight();            //saving the original image size and ratio
+        double imageRatio = oldImageWidth / oldImageHeight;
+
+        return (obs, ov, nv) -> {
+            double paneWidth = imagePane.getWidth();
+            double paneHeight = imagePane.getHeight();
+
+            double paneRatio = paneWidth / paneHeight;                                          //calculating the new pane's ratio
+            //after width or height changed
+            double newImageWidth = oldImageWidth, newImageHeight = oldImageHeight;
+
+            if (paneRatio > imageRatio) {
+                newImageHeight = oldImageWidth / paneRatio;
+            } else if (paneRatio < imageRatio) {
+                newImageWidth = oldImageHeight * paneRatio;
+            }
+
+            imageViewPrincipal.setViewport(new Rectangle2D(                                     // The rectangle used to crop
+                    (oldImageWidth - newImageWidth) / 2, (oldImageHeight - newImageHeight) / 2, //MinX and MinY to crop from the center
+                    newImageWidth, newImageHeight)                                              // new width and height
+            );
+
+            imageViewPrincipal.setFitWidth(paneWidth);
+        };
+    }
+
+
 }
